6845cf5b311780d76194dfc863ac9efec848add2
Arnaud Georgin <ageorgin@MacBook-Pro-de-Arnaud-Georgin.local>
2018-01-31 23:00:32 +0100

    Add react-native-elements dependency and start designing result list

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput, FlatList, TouchableOpacity } from 'react-native';
+import { ListItem } from "react-native-elements";
 
 export default class SearchScreen extends React.Component {
   static navigationOptions = {
@@ -8,16 +9,37 @@ export default class SearchScreen extends React.Component {
 
   constructor(props) {
     super(props)
+
     this.state = {
       searchText: "",
+      products: []
     };
   }
 
-  render() {
-    const { navigate } = this.props.navigation;
+  doSearch = () => {
+    console.log('search');
+    tmpProduct = [];
+    for (i = 0; i < 25; i++) {
+      tmpProduct.push(
+        {
+          key: i,
+          name: 'Product ' + i,
+          category: 'Product category ' + i,
+          picture: 'https://randomuser.me/api/portraits/thumb/men/53.jpg'
+        }
+      );
+    }
+    this.setState({products: tmpProduct});
+  }
 
+  navigateToProduct = (item) => {
+    const { navigate } = this.props.navigation;
+    navigate('Product', {productName: item.name, productId: item.key})
+  }
+
+  render() {
     return (
-      <View  style={{flex: 1, flexDirection: 'column'}}>
+      <View  style={{flex: 1, flexDirection: 'column', justifyContent: 'center'}}>
         <View style={{flexDirection: 'row', justifyContent: 'center'}}>
           <TextInput
             style={{flex: 0.8}}
@@ -27,21 +49,48 @@ export default class SearchScreen extends React.Component {
           <Button
             style={{flex: 0.2}}
             title="Search"
-            onPress={() => console.log('todo')}
+            onPress={this.doSearch}
           />
         </View>
-        <Text>TODO Search screen</Text>
-
-        <View style={styles.button}>
-          <Button
-            onPress={() => navigate('Product', {productName: 'Nom du produit', productId: 12})}
-            title="Aller vers la page produit"
+        <View style={{flex: 0.8}}>
+          <FlatList
+            data={this.state.products}
+            renderItem={this.renderProductItem}
           />
         </View>
       </View>
     );
   }
+
+  renderProductItem =({item}) => (
+    <ListItem
+      roundAvatar
+      title={item.name}
+      subtitle={item.category}
+      avatar={{ uri: item.picture }}
+      containerStyle={{ borderBottomWidth: 0 }}
+      onPress={() => this.navigateToProduct(item)}
+    />
+  );
 }
+
+/**
+<TouchableOpacity>
+    <View style={{paddingLeft: 10}}>
+      <Text style={{fontSize: 20, textAlign: 'center'}}>
+        {item.key}
+      </Text>
+    </View>
+  </TouchableOpacity>
+
+
+<View style={styles.button}>
+  <Button
+    onPress={() => navigate('Product', {productName: 'Nom du produit', productId: 12})}
+    title="Aller vers la page produit"
+  />
+</View>
+*/
 
 const styles = StyleSheet.create({
   button: {
