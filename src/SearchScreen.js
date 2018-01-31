702e08c7b8b1ce354a4a32c8a4abbc42acbe39fb
ageorgin <a.georgin@meetic-corp.com>
2018-01-31 14:05:14 +0100

    Start implement SearchScreen

--- src/SearchScreen.js
+++ src/SearchScreen.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default class SearchScreen extends React.Component {
   static navigationOptions = {
       header: null
   };
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchText: "",
+    };
+  }
+
   render() {
     const { navigate } = this.props.navigation;
-    
+
     return (
-      <View>
+      <View  style={{flex: 1, flexDirection: 'column'}}>
+        <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+          <TextInput
+            style={{flex: 0.8}}
+            value={this.state.searchText}
+            onChangeText={(searchText) => this.setState({searchText})}
+          />
+          <Button
+            style={{flex: 0.2}}
+            title="Search"
+            onPress={() => console.log('todo')}
+          />
+        </View>
         <Text>TODO Search screen</Text>
+
         <View style={styles.button}>
           <Button
             onPress={() => navigate('Product', {productName: 'Nom du produit', productId: 12})}
